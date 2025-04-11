3d47229ca85f04bd2bf8e989d0580c77f012f3a6
Kanghwi <poiu978645@snu.ac.kr>
2025-04-11 14:42:27 +0900

    caltest_attendance

--- calculator.cpp
+++ calculator.cpp
@@ -1,18 +1,59 @@
 #include "calculator.h"
 #include <stdexcept>
+#include <limits>
 
 int Calculator::add(int a, int b) {
-    // TODO
+    if ((a >= -b + std::numeric_limits<int>::max()) && (b>=0)) {
+        throw std::overflow_error("Integer overflow on addition");
+    }
+
+    else if ((a <= -b + std::numeric_limits<int>::min()) && (b < 0)) {
+        throw std::overflow_error("Integer overflow on addition");
+    }
+
+    else {
+        return a + b;
+    }
 }
 
 int Calculator::sub(int a, int b) {
-    // TODO
+    if ((a >= b + std::numeric_limits<int>::max()) && (b < 0)) {
+        throw std::overflow_error("Integer overflow on subtraction");
+    }
+
+    else if ((a <= b + std::numeric_limits<int>::min()) && (b >= 0)) {
+        throw std::overflow_error("Integer overflow on subtraction");
+    }
+
+    else {
+        return a - b;
+    }
 }
 
 int Calculator::mul(int a, int b) {
-    // TODO
+    if ((a!=0) && (b!=0)) {
+        if (a >= (std::numeric_limits<int>::max() / b)) {
+            throw std::overflow_error("Integer overflow on multiplication");
+        }
+
+        else if (a <= (std::numeric_limits<int>::min() / b)) {
+            throw std::overflow_error("Integer overflow on multiplication");
+        }
+    }
+
+    return a*b;
 }
 
 int Calculator::div(int a, int b) {
-    // TODO
+    if (b==0) {
+        throw std::invalid_argument("Division by zero");
+    }
+
+    else if ((a == std::numeric_limits<int>::min()) && (b==-1)) {
+        throw std::overflow_error("Intger overflow on division");
+    }
+
+    else {
+        return a / b;
+    }
 }
